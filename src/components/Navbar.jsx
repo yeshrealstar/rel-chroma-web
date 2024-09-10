373387c2cd1cad47794a29ce7470dd027da56669
InfinityWaveAI <infinitywaveaisolutions@gmail.com>
2024-09-10 19:35:57 +0530

    second build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,10 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">Relational Chroma DB</div>
+      <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+      <div className="logo">
+      <img src="./vite.svg" style={{height:"45px",width:"45px"}} />Relational Chroma DB
+      </div>
       <ul className="nav-links">
         <li><a href="#features">Features</a></li>
         <li><a href="#how-it-works">How It Works</a></li>
